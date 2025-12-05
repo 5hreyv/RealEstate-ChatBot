3737319fbc7a990067590a526c6c880ebc553140
5hreyv <shreyakatare2612@gmail.com>
2025-12-05 22:17:51 +0530

    Fix API baseURL to Railway backend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://web-production-14a2c.up.railway.app/api/query/",
-
+  baseURL: "https://web-production-14a2c.up.railway.app/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
