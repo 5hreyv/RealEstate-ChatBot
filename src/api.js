570a8892303f2ebdb1be0abfc08dfbc0e828b81f
5hreyv <shreyakatare2612@gmail.com>
2025-12-05 22:11:46 +0530

    Fix API baseURL for production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://web-production-14a2c.up.railway.app/api",
+  baseURL: "https://web-production-14a2c.up.railway.app/api/query/",
 
 });
 
