58cea52a7fe4b06dede12eea19ad41f03d407fb0
5hreyv <shreyakatare2612@gmail.com>
2025-12-06 18:15:12 +0530

    Updated backend API routes + views

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import query_view, download_csv_view, download_report_view
+from . import views  # <-- REQUIRED
 
 urlpatterns = [
-    path("query/", query_view, name="query"),
-    path("download_csv/", download_csv_view, name="download_csv"),
-    path("report_pdf/", download_report_view, name="report_pdf"),
+    path("query/", views.query_api),
+    path("download_csv/", views.download_csv),
+    path("report_pdf/", views.report_pdf),
     path("localities/", views.list_localities),
-
 ]
